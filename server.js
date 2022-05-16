a52153ecbd8b1230618c031080ef678903e20fe8
jwhitehead <jwhitehead08@gmail.com>
2022-05-16 14:23:28 -0500

    deploy instructions

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const path = require('path');
 const express = require('express');
+const app = express();
+const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
