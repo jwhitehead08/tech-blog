4af0f033e50b229f622a6abd95bd4b20535d2ecd
jwhitehead <jwhitehead08@gmail.com>
2022-05-15 21:18:37 -0500

    rearraned server.js

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-const express = require('express');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
 const path = require('path');
-
-
-
+const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-
-
-
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
