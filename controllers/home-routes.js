e1f4fa43686352ea058d0186a4ce5d8f310ab8c2
jwhitehead <jwhitehead08@gmail.com>
2022-05-20 14:55:22 -0500

    capital I for index

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User } = require('../models');
+const { Post, User } = require('../models/Index');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
